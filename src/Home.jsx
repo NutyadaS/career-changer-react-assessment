bd304b93fb6771ca97f4b14413d420e25475c5c3
NutyadaS <nutyada.s7810@gmail.com>
2023-04-18 15:24:18 +0200

    update home jsx fixing layout

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,7 @@
-import Layout from "./Layout";
 import React, { useState } from "react";
-import User from "./User";
+import Layout from "./Layout";
 import Admin from "./Admin";
+import User from "./User";
 
 const mockEmployees = [
   {
@@ -20,38 +20,48 @@ const mockEmployees = [
     id: 2,
     name: "employee 2",
     lastname: "lord",
-    position: "Designer",
+    position: "Manager",
   },
 ];
 
-// const [employees, setEmployees] = useState([
+// const Home = () => {
+//   const [employees, setEmployees] = useState([...mockEmployees]);
 
-// ]);
-// const [sector, setSector] = useState();
+//   const [sector, setSector] = useState("");
 
-// const handleClickEmployees = function (event) {
-//   setEmployees(event.target.value);
+//   const handleClickAdmin = () => {
+//     setSector(sector === "admin" ? "" : "admin");
+//   };
+
+//   const handleClickUser = () => {
+//     setSector(sector === "user" ? "" : "user");
+//   };
+
+//   return (
+//     <Layout>
+//       <h1>Generation Thailand</h1>
+//       {sector === "user" ? (
+//         <h1>Home - User Sector</h1>
+//       ) : sector === "admin" ? (
+//         <h1>Home - Admin Sector</h1>
+//       ) : (
+//         <h1>Home - React Assessment</h1>
+//       )}
+//       <button onClick={handleClickUser}>User Home sector</button>
+//       <button onClick={handleClickAdmin}>Admin Home sector</button>
+//       {sector === "user" && <User employees={employees} />}
+//       {sector === "admin" && (
+//         <Admin employees={employees} setEmployees={setEmployees} />
+//       )}
+//     </Layout>
+//   );
 // };
 
-// function handleClickUser() {
-//   User.push("./User");
-// }
-
-// export default function Home() {
-//   const navigate = useNavigate();
-//   navigate("/");
-
-//   // const Home = () => {
-//   const navigateToUser = () => {
-//     navigate("/user");
-//   };
-
-//   const navigateToAdmin = () => {
-//     navigate("/admin");
-//   };
+// export default Home;
 
 const Home = () => {
   const [employees, setEmployees] = useState([...mockEmployees]);
+
   const [sector, setSector] = useState("");
 
   const handleClickAdmin = () => {
@@ -63,23 +73,26 @@ const Home = () => {
   };
 
   return (
-    <Layout>
+    // <Layout>
+    <>
       <h1>Generation Thailand</h1>
+
       {sector === "user" ? (
         <h1>Home - User Sector</h1>
       ) : sector === "admin" ? (
         <h1>Home - Admin Sector</h1>
       ) : (
-        <h1>React - Assessment</h1>
+        <h1>Home - React Assessment</h1>
       )}
-
-      <button onClick={handleClickUser}>User Home Sector</button>
-      <button onClick={handleClickAdmin}>Admin Home Sector</button>
+      <button onClick={handleClickUser}>User Home sector</button>
+      <button onClick={handleClickAdmin}>Admin Home sector</button>
       {sector === "user" && <User employees={employees} />}
       {sector === "admin" && (
         <Admin employees={employees} setEmployees={setEmployees} />
       )}
-    </Layout>
+      {/* </Layout> */}
+    </>
   );
 };
+
 export default Home;
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./Layout.css";
+import "./Styles.css";
 
 const Navbar = () => {
   return (
